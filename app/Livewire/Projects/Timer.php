2ead06c5d3b29750fbce567cbbcb4887c9c86499
P.S.O <paulosergiooliveira113@gmail.com>
2024-10-14 16:16:47 -0300

    Avisando Criação de Proposta

---
+++ app/Livewire/Projects/Timer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use App\Models\Project;
+use Livewire\Component;
+
+class Timer extends Component
+{
+    public Project $project;
+
+    public function timer()
+    {
+
+    }
+
+    public function render()
+    {
+        $diff = now()->diff($this->project->end_at);
+
+        return view('livewire.projects.timer', [
+            'days' => $diff->d,
+            'hours' => $diff->h,
+            'minutes' => $diff->i,
+            'seconds' => $diff->s
+        ]);
+        
+    }
+}
